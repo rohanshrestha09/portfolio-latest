5da2c975932fc87585f6bcb6aab1626c025fc5cb
rohanshrestha09 <shresthacr11@gmail.com>
2023-04-02 22:51:06 +0545

    Added animation to navbar and glass effect

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,51 +1,104 @@
 import { Space_Grotesk } from 'next/font/google';
 import { useRouter } from 'next/router';
+import { useEffect, useRef, useState } from 'react';
 
 const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] });
 
 interface Props {
    navbar: {
-      children: { name: string; target: string }[];
       className?: string;
+      items: { name: string; target: string }[];
    };
    content: {
-      children: React.ReactNode;
+      id?: string;
       className?: string;
+      children: React.ReactNode;
    };
    layout: {
+      className?: string;
       children: React.ReactNode;
    };
 }
 
-function Navbar({ className, children }: Props['navbar']) {
-   const { asPath, push } = useRouter();
+function Navbar({ className, items }: Props['navbar']) {
+   const { asPath } = useRouter();
+
+   const [path, setPath] = useState('');
+
+   const navbarRef = useRef<HTMLDivElement>(null);
+
+   useEffect(() => {
+      setPath(asPath === '/' ? 'about' : asPath.replaceAll('/#', ''));
+   }, [asPath]);
+
+   useEffect(() => {
+      let lastScroll = 0;
+
+      let MAX_SCROLL = 984;
+
+      window.onscroll = function () {
+         let currentScroll =
+            document.documentElement.scrollTop || document.body.scrollTop;
+
+         if (currentScroll > 100)
+            navbarRef.current?.classList.add('bg-[rgba(27,27,27,0.1)]');
+         else navbarRef.current?.classList.remove('bg-[rgba(27,27,27,0.1)]');
+
+         if (currentScroll > 0 && lastScroll <= currentScroll) {
+            lastScroll = currentScroll;
+
+            if (navbarRef.current && currentScroll < MAX_SCROLL)
+               navbarRef.current.style.right = currentScroll / 28.11 + '%';
+         } else {
+            lastScroll = currentScroll;
+
+            if (navbarRef.current && currentScroll < MAX_SCROLL)
+               navbarRef.current.style.right = currentScroll / 28.11 + '%';
+         }
+      };
+   }, []);
 
    return (
       <nav
-         className={`fixed right-[80px] top-[50px] flex gap-[40px] ${className}`}
+         className={`navbar fixed right-0 top-[35px] mx-[2.45%] flex gap-[40px] rounded-full px-[35px] py-[15px] ${
+            className ?? ''
+         }`}
+         ref={navbarRef}
       >
-         {children.map(({ name, target }) => (
-            <p
+         {items.map(({ name, target }) => (
+            <a
                key={name}
                className={`relative cursor-pointer pb-[4px] text-xl font-semibold after:absolute after:bottom-0 after:right-0 after:h-[2.5px] after:bg-white after:transition-all after:duration-200 after:content-[""] hover:after:w-3/4 ${
-                  asPath === `/#${target}` ? 'after:w-3/4' : 'after:w-0'
+                  path === target ? 'after:w-3/4' : 'after:w-0'
                }`}
-               onClick={() => push(`#${target}`)}
+               href={`#${target}`}
+               onClick={() => setPath(target)}
             >
                {name}
-            </p>
+            </a>
          ))}
       </nav>
    );
 }
 
-function Content({ className, children }: Props['content']) {
-   return <section className={`min-h-screen ${className}`}>{children}</section>;
+function Content({ className, children, ...props }: Props['content']) {
+   return (
+      <section
+         className={`min-h-screen py-[50px] ${className ?? ''}`}
+         {...props}
+      >
+         {children}
+      </section>
+   );
 }
 
-export default function Layout({ children }: Props['layout']) {
+export default function Layout({ className, children }: Props['layout']) {
    return (
-      <main className={`${spaceGrotesk.className} bg-[#000101] text-white`}>
+      <main
+         className={`${
+            spaceGrotesk.className
+         } bg-[#000101] px-[250px] text-white ${className ?? ''}`}
+      >
          {children}
       </main>
    );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,41 @@
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 import Layout from 'components/Layout';
 
 export default function Home() {
-   const router = useRouter();
-
    return (
       <Layout>
          <Head>
             <title>Rohan Shrestha</title>
          </Head>
 
-         <Layout.Content className='relative flex flex-col items-center justify-center gap-[48px]'>
+         <Layout.Navbar
+            items={[
+               { name: 'About', target: 'about' },
+               { name: 'Tools', target: 'tools' },
+               { name: 'Projects', target: 'projects' },
+               { name: 'Contact', target: 'contact' },
+            ]}
+         />
+
+         <Layout.Content className='flex flex-col items-center justify-center gap-[48px]'>
             <p className='animated-text w-max text-8xl font-black'>
                I&apos;m Rohan Shrestha
             </p>
 
             <p className='text-7xl font-semibold'>Fullstack Developer</p>
 
-            <button
-               className='rounded-md border-[1.5px] border-white bg-white px-[16px] py-[10px] text-xl font-medium text-black transition-all duration-150 hover:bg-inherit hover:text-white'
-               onClick={() => router.push('/#about')}
-            >
-               whoami?
-            </button>
+            <a href='#about' className='text-xl'>
+               <button className='rounded-md border-[1.5px] border-white bg-white px-[16px] py-[10px] font-medium text-black transition-all duration-150 hover:bg-inherit hover:text-white'>
+                  whoami?
+               </button>
+            </a>
+         </Layout.Content>
 
-            <Layout.Navbar>
-               {[
-                  { name: 'About', target: 'about' },
-                  { name: 'Tools', target: 'tools' },
-                  { name: 'Projects', target: 'projects' },
-                  { name: 'Contact', target: 'contact' },
-               ]}
-            </Layout.Navbar>
+         <Layout.Content
+            id='about'
+            className='flex flex-col justify-end gap-10'
+         >
+            <p className='w-fit text-5xl font-semibold'>About Me</p>
          </Layout.Content>
       </Layout>
    );
